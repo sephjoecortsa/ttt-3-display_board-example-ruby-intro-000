aad7e18c41c9d8623ab8fb906df259320f8d7ad5
Joseph Castro <sephjoecortsa@outlook.com>
2021-02-05 18:06:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,10 @@
 def display_board
-  puts "A Tic Tac Toe Board"
+puts "A Tic Tac Toe Board"
+
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+
 end
